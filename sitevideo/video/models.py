f309b0006a3ce537ae510ca7452a6449a3090635
alexxgusse <gusev709.com@yandex.ru>
2024-02-26 21:07:45 +0500

    added VideoFile table

--- sitevideo/video/models.py
+++ sitevideo/video/models.py
@@ -1,3 +1,14 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+
+class VideoFile(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, verbose_name="UUID")
+    filename = models.CharField(max_length=255, verbose_name="Название файла")
+    processing = models.BooleanField(default=False, verbose_name='Идет ли процесс обработки')
+    processingSuccess = models.BooleanField(default=None, null=True,
+                                            verbose_name="Успешность последней операции над видео")
+    filepath = models.FileField(upload_to='video_file/%Y/%m/%d/')
+
