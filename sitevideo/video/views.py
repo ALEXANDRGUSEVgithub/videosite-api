d535d52d917768caadc0994bc6a6786fa00c0d32
alexxgusse <gusev709.com@yandex.ru>
2024-02-28 18:30:26 +0500

    improved path method. added normalization of the file path, deletion of the old file. Comments added

--- sitevideo/video/views.py
+++ sitevideo/video/views.py
@@ -4,12 +4,13 @@ from django.http import JsonResponse
 from rest_framework import generics, status
 from rest_framework.response import Response
 
+from sitevideo.settings import MEDIA_ROOT
 from .models import VideoFile
 from .serializers import VideoFileSerializer, VideoFileCreateSerializer
 import os
 
 
-class VideoFileListAPIView(generics.RetrieveUpdateDestroyAPIView ):
+class VideoFileListAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = VideoFile.objects.all()
     serializer_class = VideoFileSerializer
 
@@ -41,16 +42,20 @@ class VideoFileListAPIView(generics.RetrieveUpdateDestroyAPIView ):
         width = request.data.get('width')
         height = request.data.get('height')
 
+        # Проверка на корректность полученных данных width и height
         if not (width and height and isinstance(width, int) and isinstance(height, int) and width > 20 and height > 20):
             return JsonResponse({'error': 'Неверные данные для изменения разрешения'},
                                 status=status.HTTP_400_BAD_REQUEST)
 
+        # Устанавливаем статусы процесса обработки
         instance.processing = True
         instance.processingSuccess = False
         instance.save()
 
+        # Получаем абсолютный входной путь файла
         input_file_path = str(instance.filepath.path)
 
+        # Создаем выходной путь файла
         output_file_path = f'{os.path.splitext(input_file_path)[0]}_resized.mp4'
 
         try:
@@ -58,10 +63,24 @@ class VideoFileListAPIView(generics.RetrieveUpdateDestroyAPIView ):
 
             instance.processingSuccess = True
         except Exception as e:
+            instance.processing = False
             instance.processingSuccess = False
             return JsonResponse({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
         finally:
             instance.processing = False
+
+            # Приводим путь обработанного файла к относительному виду
+            final_file_path = os.path.relpath(output_file_path, MEDIA_ROOT)
+
+            # Нормализуем путь
+            final_file_path = final_file_path.replace("\\", "/")
+
+            # Сохраняем в базе данных новый путь
+            instance.filepath = final_file_path
+
+            # Удаляем старый необработанный файл
+            os.remove(input_file_path)
+
             instance.save()
 
         return JsonResponse({'success': True})
@@ -80,5 +99,3 @@ class VideoFileCreateAPIView(generics.CreateAPIView):
         response_data = {'id': serializer.instance.id}
         headers = self.get_success_headers(serializer.data)
         return Response(response_data, status=status.HTTP_200_OK)
-
-
